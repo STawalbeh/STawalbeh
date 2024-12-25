90bc1038c889780d0c10e27fb0b09725802e9702
Saja Tawalbeh <82958017+STawalbeh@users.noreply.github.com>
2024-12-25 22:56:27 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,20 +35,24 @@
     <div class="column">
         <div class="box">
             <div class="content">
-                <p> I'm Saja Tawalbeh! ... </p>
+                <p> My name is Saja Tawalbeh. I received my bachelor’s in computer science in Jordan. Then, I completed my master’s in AI, specifically in natural language processing, and worked as a research assistant in the same area for two years at Jordan University of Science and  Technology. </p>
 
                 <p> 
-                    My research focusses on .....
-                    ........
-                </p>
-
-                <p> 
-                    .......
+                    I'm currently a senior research fellow and pursuing my Ph.D. in Explainable Artificial Intelligence (XAI) at the Faculty of Science, University of Antwerp, sqIRL/IDLab, imec, Antwerp, Belgium.
+                    Additionally, she is a lecturer in the Department of Applied Computer Science at Karel de Grote University of Applied Sciences, Antwerp, Belgium.
+                    
                 </p>
 
                 <p>
-                    ......
+                    My research interests include NLP, computer vision, and XAI, with a focus on Capsule Networks, Convolutional Neural Networks, and text-based analysis.
                 </p>
+
+
+                <p> 
+                     I’m passionate about photography, playing chess, and exploring new places on long walks, which helps me feel more connected to the world around me. 
+                     Walking also proves to be an excellent way to brainstorm new ideas or troubleshoot research-related challenges
+                </p>
+
             </div>
         </div>
     </div>
