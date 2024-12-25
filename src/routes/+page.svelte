9deb45b4f5197b4b490de449ce28b67be032e6a1
Saja Tawalbeh <82958017+STawalbeh@users.noreply.github.com>
2024-12-25 22:02:01 +0100

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import Navbar from "$lib/navbar.svelte";
+</script>
+
+<Navbar />
+
+<div class="columns">
+    <div class="column is-3">
+        <div class="box has-text-centered">
+            <figure class="image is-128x128 is-inline-block">
+                <img class="is-rounded" src="profile.jpg" alt="me :)" />
+            </figure>
+            <h4 class="subtitle is-4"> <b> Saja Tawalbeh </b> </h4>
+            <!-- <h6 class="subtitle is-6"> Antwerp, Belgium </h6> -->
+
+            <h6 class="subtitle is-6"> Machine Learning Researcher </h6>
+
+            <hr />
+            <div class="block">
+                <span class="icon"><i class="fas fa-envelope"></i> </span>
+                <span> <a href="mailto:sajatawalbeh91@gmail.com"> Email </span>
+            </div>
+
+            <div class="block">
+                <span class="icon"><i class="fab fa-linkedin"></i> </span>
+                <span> <a href="https://www.linkedin.com/in/saja-tawalbeh-b744a1151/"> LinkedIn </a> </span>
+            </div>
+            
+            <div class="block">
+                <span class="icon"><i class="fab fa-github"></i> </span>
+                <span> <a href="https://github.com/STawalbeh"> GitHub </a> </span>
+            </div>
+        </div>
+    </div>
+    <div class="column">
+        <div class="box">
+            <div class="content">
+                <p> I'm Saja Tawalbeh! ... </p>
+
+                <p> 
+                    My research focusses on .....
+                    ........
+                </p>
+
+                <p> 
+                    .......
+                </p>
+
+                <p>
+                    ......
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
